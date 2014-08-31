4e4ba098d69bf68edece3346b36dc3b52fe6655b
Dreaded_X <tim.huizinga@gmail.com>
2014-08-31 15:49:07 +0200

    Update install script to also be able to install gnome-terminal-colors-solarized

--- install.sh
+++ install.sh
@@ -10,10 +10,21 @@ if [ ! -f ~/.vimrc ]; then ln -s ~/.dotfiles/vimrc ~/.vimrc; fi
 if [ ! -d ~/.vim ]; then ln -s ~/.dotfiles/vim ~/.vim; fi
 if [ ! -f ~/.gitignore ]; then ln -s ~/.dotfiles/gitignore ~/.gitignore; fi
 
-#ln -s ~/.dotfiles/zshrc ~/.zshrc
-#ln -s ~/.dotfiles/vimrc ~/.vimrc
-#ln -s ~/.dotfiles/vim ~/.vim
-#ln -s ~/.dotfiles/gitignore ~/.gitignore
+#install gnome-terminal-colors-solarized
+while true; do
+    read -p "Do you wish to install gnome-terminal-colors-solarized? " yn
+    case $yn in
+        [Yy]* ) sudo apt-get install dconf-cli;
+		git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+		cd gnome-terminal-colors-solarized;
+		./install.sh;
+		cd ..;
+		rm -rf gnome-terminal-colors-solarized;
+		break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 if [ ! -f /usr/local/bin/tmux-vim-select-pane ]; then
 	curl -fsSL https://raw.github.com/mislav/dotfiles/1500cd2/bin/tmux-vim-select-pane -o /usr/local/bin/tmux-vim-select-pane;
