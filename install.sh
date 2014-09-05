a875ab42458220462d34f44501f6fdbf55c553fc
Dreaded_X <tim.huizinga@gmail.com>
2014-09-05 21:40:23 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,30 @@ if [ $EUID != 0 ]; then
 	exit $?
 fi
 
-if [ ! -f ~/.tmux.conf ]; then ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf; fi
-if [ ! -f ~/.zshrc ]; then ln -s ~/.dotfiles/zshrc ~/.zshrc; fi
-if [ ! -f ~/.vimrc ]; then ln -s ~/.dotfiles/vimrc ~/.vimrc; fi
-if [ ! -d ~/.vim ]; then ln -s ~/.dotfiles/vim ~/.vim; fi
-if [ ! -f ~/.gitignore ]; then ln -s ~/.dotfiles/gitignore ~/.gitignore; fi
-
-#install gnome-terminal-colors-solarized
 while true; do
-    read -p "Do you wish to install gnome-terminal-colors-solarized? " yn1
-    case $yn1 in
-        [Yy]* ) sudo apt-get install dconf-cli;
-		git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+	read -p "Do you want to make symlinks? " yn
+	case $yn in
+	    [Yy]* ) if [ ! -f ~/.tmux.conf ]; then ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf; fi
+		    if [ ! -f ~/.zshrc ]; then ln -s ~/.dotfiles/zshrc ~/.zshrc; fi
+		    if [ ! -f ~/.vimrc ]; then ln -s ~/.dotfiles/vimrc ~/.vimrc; fi
+		    if [ ! -d ~/.vim ]; then ln -s ~/.dotfiles/vim ~/.vim; fi
+		    if [ ! -f ~/.gitignore ]; then ln -s ~/.dotfiles/gitignore ~/.gitignore; fi
+		    echo "Done...";
+		    break;;
+	    [Nn]* ) break;;
+    	    * ) echo "Please answer yes or no.";;
+	esac
+done
+
+while true; do
+	read -p "Do you wish to install gnome-terminal-colors-solarized? (requires dconf-cli) " yn
+    case $yn in
+        [Yy]* ) git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git;
 		cd gnome-terminal-colors-solarized;
-		./install.sh;
+		./install.sh -s 1;
 		cd ..;
 		rm -rf gnome-terminal-colors-solarized;
+		echo "Done...";
 		break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
@@ -27,9 +35,10 @@ while true; do
 done
 
 while true; do
-    read -p "Do you wish to update all submodules? " yn2
-    case $yn2 in
-        [Yy]* ) git submodule update --init --recursive
+    read -p "Do you wish to update all submodules? " yn
+    case $yn in
+        [Yy]* ) git submodule update --init --recursive;
+		echo "Done...";
 		break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
@@ -37,9 +46,10 @@ while true; do
 done
 
 while true; do
-    read -p "Do you wish to compile YCM? " yn3
-    case $yn3 in
-        [Yy]* ) ./vim/bundle/YouCompleteMe/install.sh
+    read -p "Do you wish to compile YCM? " yn
+    case $yn in
+        [Yy]* ) ./vim/bundle/YouCompleteMe/install.sh;
+		echo "Done...";
 		break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
@@ -47,21 +57,41 @@ while true; do
 done
 
 while true; do
-    read -p "Do you wish to install CTags? " yn3
-    case $yn3 in
-        [Yy]* ) sudo apt-get install exuberant-ctags 
+    read -p "Do you wish to install CTags? " yn
+    case $yn in
+        [Yy]* ) sudo apt-get install exuberant-ctags;
+		echo "Done...";
 		break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-git submodule update --init --recursive
+while true; do
+    read -p "Do you wish to install powerline? " yn
+    case $yn in
+        [Yy]* ) pip install --user git+git://github.com/Lokaltog/powerline;
+	        wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf;
+		mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/;
+		fc-cache -vf ~/.fonts;
+		mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/;
+		echo "Done...";
+		break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
+while true; do
+    read -p "Do you wish to install tmux-vim-select-pane? " yn
+    case $yn in
+        [Yy]* ) curl -fsSL https://raw.github.com/mislav/dotfiles/1500cd2/bin/tmux-vim-select-pane -o /usr/local/bin/tmux-vim-select-pane;
+		chmod +x /usr/local/bin/tmux-vim-select-pane;
+		echo "Done...";
+		break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
-if [ ! -f /usr/local/bin/tmux-vim-select-pane ]; then
-	curl -fsSL https://raw.github.com/mislav/dotfiles/1500cd2/bin/tmux-vim-select-pane -o /usr/local/bin/tmux-vim-select-pane;
-	chmod +x /usr/local/bin/tmux-vim-select-pane;
-fi
-
-echo "Done installing..."
+echo "Done installing...";
