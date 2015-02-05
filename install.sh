2161454523855e00635d66402229249903d5ec0f
Dreaded_X <tim.huizinga@gmail.com>
2015-02-05 17:49:52 +0100

    Updated install script and made patch folder more organized

--- install.sh
+++ install.sh
@@ -1,92 +1,69 @@
 #!/bin/bash
-# TODO: Add eclim/eclipse installer
-while true; do
-  read -p "Do you want to make symlinks? " yn
-  case $yn in
-    [Yy]* ) if [ ! -f ~/.tmux.conf ]; then ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf; fi
-      if [ ! -f ~/.zshrc ]; then ln -s ~/.dotfiles/zshrc ~/.zshrc; fi
-      if [ ! -f ~/.vimrc ]; then ln -s ~/.dotfiles/vimrc ~/.vimrc; fi
-      if [ ! -d ~/.vim ]; then ln -s ~/.dotfiles/vim ~/.vim; fi
-      if [ ! -f ~/.gitignore ]; then ln -s ~/.dotfiles/gitignore ~/.gitignore; fi
-      if [ ! -f ~/.yaourtrc ]; then ln -s ~/.dotfiles/yaourtrc ~/.yaourtrc; fi
-      echo "Done...";
-      break;;
-    [Nn]* ) break;;
-  * ) echo "Please answer yes or no.";;
-esac
-done
 
-# while true; do
-#   read -p "Do you wish to install gnome-terminal-colors-solarized? (requires dconf-cli) " yn
-#   case $yn in
-#     [Yy]* ) cd ..; 
-#       git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git;
-#       cd gnome-terminal-colors-solarized;
-#       ./install.sh;
-#       cd ..;
-#       rm -rf gnome-terminal-colors-solarized;
-#       cd .dotfiles;
-#       echo "Done...";
-#       break;;
-#     [Nn]* ) break;;
-#   * ) echo "Please answer yes or no.";;
-# esac
-# done
+dotfiles="$HOME/$1"
 
-while true; do
-  read -p "Do you wish to update all submodules? " yn
-  case $yn in
-    [Yy]* ) git submodule update --init --recursive;
-      echo "Done...";
-      break;;
-    [Nn]* ) break;;
-  * ) echo "Please answer yes or no.";;
-esac
-done
+# Show help
+function show_help {
+  echo "Specify directory";
+}
 
-while true; do
-  read -p "Do you wish to compile YCM? " yn
-  case $yn in
-    [Yy]* ) ./vim/bundle/YouCompleteMe/install.sh;
-      echo "Done...";
-      break;;
-    [Nn]* ) break;;
-  * ) echo "Please answer yes or no.";;
-esac
-done
+# Ask user
+function ask {
+  while true; do
+    read -p "$1 [Y/n] " yn
+    case $yn in
+      * ) $2; break;;
+      [Nn]* ) break;;
+    esac
+  done
+}
 
-# while true; do
-#   read -p "Do you wish to install source code pro with powerline? " yn
-#   case $yn in
-#     [Yy]* )	ssudo curl -fsSL https://github.com/Lokaltog/powerline-fonts/raw/master/SourceCodePro/Sauce%20Code%20Powerline%20Regular.otf -o /usr/local/bin/tmux-vim-select-pane;      
-#       echo "Done...";
-#       break;;
-#     [Nn]* ) break;;
-#   * ) echo "Please answer yes or no.";;
-# esac
-# done
-#
-# while true; do
-#   read -p "Do you wish to install tmux-vim-select-pane? " yn
-#   case $yn in
-#     [Yy]* )	sudo curl -fsSL https://raw.github.com/mislav/dotfiles/1500cd2/bin/tmux-vim-select-pane -o /usr/local/bin/tmux-vim-select-pane;
-#       sudo chmod +x /usr/local/bin/tmux-vim-select-pane;
-#       echo "Done...";
-#       break;;
-#     [Nn]* ) break;;
-#   * ) echo "Please answer yes or no.";;
-# esac
-# done
-#
-# while true; do
-#   read -p "Do you wish to install CTags? " yn
-#   case $yn in
-#     [Yy]* )	sudo apt-get install exuberant-ctags;
-#       echo "Done...";
-#       break;;
-#     [Nn]* ) break;;
-#   * ) echo "Please answer yes or no.";;
-# esac
-# done
+# Update submodules
+function submodule {
+  git submodule update --init --recursive
+}
+# Apply patches
+function patch {
+  cd $dotfiles/oh-my-zsh
+  git am $dotfiles/patches/oh-my-zsh/*.patch
+}
 
-echo "Done installing...";
+# Comple YouCompleteMe
+function ycm {
+  $dotfiles/vim/bundle/YouCompleteMe/install.sh
+}
+
+# Install tmux-vim-select-pane
+function tvpane {
+  sudo curl -fsSL https://raw.github.com/mislav/dotfiles/1500cd2/bin/tmux-vim-select-pane -o /usr/local/bin/tmux-vim-select-pane
+  sudo chmod +x /usr/local/bin/tmux-vim-select-pane
+}
+
+# Install fonts
+function fonts {
+  curl -fsSL https://github.com/Lokaltog/powerline-fonts/raw/master/SourceCodePro/Sauce%20Code%20Powerline%20Regular.otf -o "$HOME/.fonts/Sauce Code Powerline Regular.otf"
+}
+
+# Setup symlinks to config files
+function symlink {
+  if [ ! -f ~/.tmux.conf ]; then ln -s $dotfiles/tmux.conf ~/.tmux.conf; fi
+  if [ ! -f ~/.zshrc ]; then ln -s $dotfiles/zshrc ~/.zshrc; fi
+  if [ ! -f ~/.vimrc ]; then ln -s $dotfiles/vimrc ~/.vimrc; fi
+  if [ ! -f ~/.yaourtrc ]; then ln -s $dotfiles/yaourtrc ~/.yaourtrc; fi
+  if [ ! -d ~/.vim ]; then ln -s $dotfiles/vim ~/.vim; fi
+  if [ ! -d ~/.local/bin/scripts ]; then ln -s $dotfiles/scripts ~/.local/bin/scripts; fi
+  echo "All symlink have been made"
+}
+
+if [ -z "$1" ]; then show_help; exit 0; fi; 
+
+cd $dotfiles
+
+# Ask and execute the install script
+ask "Update submodules" submodule
+ask "Patch submodules" patch
+ask "Compile YCM" ycm
+ask "Install tmux-vim-select-pane" tvpane
+ask "Install fonts" fonts
+# ask "Install gnome-terminal theme"
+ask "Make symlinks" symlink
