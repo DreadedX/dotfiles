cf6ffdb83a459f5726e881b3e68717b03e041827
Dreaded_X <tim@huizinga.dev>
2024-04-13 18:13:36 +0200

    Switch to async formatting automatically if the formatter is slow

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,5 @@
 -- https://github.com/stevearc/conform.nvim
+local slow_format_filetypes = {}
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
@@ -35,8 +36,25 @@ return {
 			-- ["*"] = { "codespell" },
 			["_"] = { "trim_whitespace", "trim_newlines" },
 		},
+		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			if slow_format_filetypes[vim.bo[bufnr].filetype] then
+				return
+			end
+			local function on_format(err)
+				if err and err:match("timeout$") then
+					slow_format_filetypes[vim.bo[bufnr].filetype] = true
+				end
+			end
+			return { timeout_ms = 200, lsp_fallback = true }, on_format
+		end,
 		format_after_save = function(bufnr)
 			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			if not slow_format_filetypes[vim.bo[bufnr].filetype] then
 				return
 			end
 			return { lsp_fallback = true }
