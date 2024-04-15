b361c8863e85821c33f7bd5c4b513c6d27c5c84e
Dreaded_X <tim@huizinga.dev>
2024-04-15 05:21:35 +0200

    Added styling to Telescope and floating windows

--- nvim/dot-config/nvim/lua/themes/gruvbox.lua
+++ nvim/dot-config/nvim/lua/themes/gruvbox.lua
@@ -1,13 +1,13 @@
 -- https://github.com/ellisonleao/gruvbox.nvim
 return {
-	'ellisonleao/gruvbox.nvim',
+	"ellisonleao/gruvbox.nvim",
 	commit = "487598d979868224aff92cf8818195c1a60e5dfe", -- Commit before things start breaking
 	priority = 1000,
 	config = function()
 		local config = require("gruvbox").config
 		local colors = require("gruvbox.palette").get_base_colors(vim.o.background, config.contrast)
 
-		require('gruvbox').setup {
+		require("gruvbox").setup({
 			background = "dark",
 			italic = {
 				strings = false,
@@ -16,9 +16,21 @@ return {
 				YankHighlight = { fg = colors.blue, bg = colors.bg0, reverse = config.inverse },
 				IncSearch = { fg = colors.aqua },
 				Search = { fg = colors.aqua },
-			}
-		}
+				NormalFloat = { bg = colors.bg0 },
+				FloatBorder = { fg = colors.bg4 },
+				TelescopePromptTitle = { fg = colors.fg4 },
+				TelescopePromptBorder = { fg = colors.bg4 },
+				TelescopePromptPrefix = { fg = colors.red },
+				TelescopePreviewTitle = { fg = colors.fg4 },
+				TelescopePreviewBorder = { fg = colors.bg4 },
+				TelescopeResultsTitle = { fg = colors.fg4 },
+				TelescopeResultsBorder = { fg = colors.bg4 },
+				TelescopeMatching = { fg = colors.aqua, bold = true },
+				TelescopeSelection = { fg = colors.blue },
+				TelescopeSelectionCaret = { fg = colors.red },
+			},
+		})
 
-		vim.cmd.colorscheme 'gruvbox'
+		vim.cmd.colorscheme("gruvbox")
 	end,
 }
