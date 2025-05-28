0c074e2aecf6d220374c0835ce3312492a2145ce
Dreaded_X <tim@huizinga.dev>
2025-05-28 19:23:06 +0200

    Use treesitter to set folds

--- nvim/dot-config/nvim/lua/options.lua
+++ nvim/dot-config/nvim/lua/options.lua
@@ -66,6 +66,8 @@ vim.o.listchars = "trail:~,tab:¦⁃,nbsp:␣"
 vim.o.list = true
 
 -- Fold settings
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldlevelstart = 99
 
 -- Windows borders
