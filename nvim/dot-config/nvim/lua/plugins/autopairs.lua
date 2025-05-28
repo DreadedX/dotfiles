92f62551d9d23fe8328d84cca17f7e52644fdf1b
Dreaded_X <tim@huizinga.dev>
2025-05-28 19:05:32 +0200

    Switched to different auto pair plugin

--- nvim/dot-config/nvim/lua/plugins/autopairs.lua
+++ nvim/dot-config/nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,27 @@
--- https://github.com/windwp/nvim-autopairs
+-- https://github.com/altermo/ultimate-autopair.nvim
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = true,
+	"altermo/ultimate-autopair.nvim",
+	event = { "InsertEnter", "CmdlineEnter" },
+	branch = "v0.6",
+	opts = {
+		fastwarp = {
+			faster = true,
+		},
+		-- Fix single quotes in rust
+		config_internal_pairs = {
+			{
+				"'",
+				"'",
+				multiline = false,
+				nft = {},
+				suround = true,
+				cond = function(fn)
+					if fn.get_ft() ~= "rust" then
+						return true
+					end
+					return not fn.in_node({ "bounded_type", "reference_type", "type_arguments", "type_parameters" })
+				end,
+			},
+		},
+	},
 }
