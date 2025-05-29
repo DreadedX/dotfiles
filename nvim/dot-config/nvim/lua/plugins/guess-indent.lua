7d71427f28c6d2faa142fb8c243c9ef2ba75d431
Dreaded_X <tim@huizinga.dev>
2025-05-29 21:57:36 +0200

    Auto run geuss indent

--- nvim/dot-config/nvim/lua/plugins/guess-indent.lua
+++ nvim/dot-config/nvim/lua/plugins/guess-indent.lua
@@ -3,10 +3,5 @@ return {
 	-- Adds a command to automatically detect the indentation settings
 	-- Prefer to use .editorconfig for projects and modeline for files
 	"NMAC427/guess-indent.nvim",
-	keys = {
-		{ "<leader>gi", "<cmd>GuessIndent<cr>", desc = "Guess indentation" },
-	},
-	opts = {
-		auto_cmd = false,
-	},
+	opts = {},
 }
