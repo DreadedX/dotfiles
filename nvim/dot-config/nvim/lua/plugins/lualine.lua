3f9ec6a29fe7be72b278ce77be57fc684ea60b2c
Dreaded_X <tim@huizinga.dev>
2025-05-30 05:59:01 +0200

    Switch from yaml-companion to schema-companion

--- nvim/dot-config/nvim/lua/plugins/lualine.lua
+++ nvim/dot-config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,11 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 
 local function get_schema()
-	local schema = require("yaml-companion").get_buf_schema(0)
-	if schema.result[1].name == "none" then
+	local schema = require("schema-companion.context").get_buffer_schema()
+	if schema.name == "none" then
 		return ""
 	end
-	return schema.result[1].name
+	return schema.name
 end
 
 return {
---
+++ nvim/dot-config/nvim/lua/plugins/schema-companion.lua
@@ -0,0 +1,63 @@
+local kubernetes = {}
+kubernetes.name = "Kubernetes"
+
+---@type schema_companion.MatcherMatchFn
+kubernetes.match = function(bufnr)
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	local kind = false
+	local api_version = false
+
+	for _, line in ipairs(lines) do
+		if kind or vim.regex("^kind: .*$"):match_str(line) then
+			kind = true
+		end
+
+		if api_version or vim.regex("^apiVersion: .*$"):match_str(line) then
+			api_version = true
+		end
+
+		if kind and api_version then
+			return {
+				name = "Kubernetes",
+				uri = require("kubernetes").yamlls_schema(),
+			}
+		end
+	end
+
+	return nil
+end
+
+return {
+	"cenk1cenk2/schema-companion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"diogo464/kubernetes.nvim",
+	},
+	config = function()
+		require("schema-companion").setup({
+			enable_telescope = true,
+			matchers = {
+				kubernetes,
+			},
+			schemas = {},
+		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>ss",
+			require("telescope").extensions.schema_companion.select_schema,
+			{ desc = "Select schema" }
+		)
+
+		vim.lsp.config(
+			"yamlls",
+			require("schema-companion").setup_client({
+				single_file_support = true,
+				settings = {
+					yaml = {},
+				},
+			})
+		)
+	end,
+}
