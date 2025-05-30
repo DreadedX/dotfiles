e61c059de6ee452192e59749913282f047b1f4dd
Dreaded_X <tim@huizinga.dev>
2025-05-31 01:13:01 +0200

    Proper lazy load fix for colorizer

--- nvim/dot-config/nvim/lua/plugins/colorizer.lua
+++ nvim/dot-config/nvim/lua/plugins/colorizer.lua
@@ -5,6 +5,7 @@ return {
 	"NvChad/nvim-colorizer.lua",
 	event = "VeryLazy",
 	opts = {
+		lazy_load = true,
 		filetypes = {
 			"*", -- Enable color highlighting for all files
 			"!neo-tree", -- Exclude neo-tree
@@ -19,16 +20,4 @@ return {
 			names = false,
 		},
 	},
-	-- Fix lazy loading: https://github.com/NvChad/nvim-colorizer.lua
-	config = function(_, opts)
-		local colorizer = require("colorizer")
-		colorizer.setup(opts)
-
-		-- nvim-colorizer doesn't work on the initial buffer if we lazy load, so force it to attach
-		-- on load.
-		local bufnr = vim.api.nvim_get_current_buf()
-		if bufnr and not colorizer.is_buffer_attached(bufnr) then
-			colorizer.attach_to_buffer(bufnr)
-		end
-	end,
 }
