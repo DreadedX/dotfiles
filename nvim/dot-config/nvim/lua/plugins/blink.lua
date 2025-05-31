525e9099e4135ef8bd379bdc419ee7533fc6ccb3
Dreaded_X <tim@huizinga.dev>
2025-05-31 05:09:16 +0200

    Cleanup in blink config

--- nvim/dot-config/nvim/lua/plugins/blink.lua
+++ nvim/dot-config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,6 @@
 --- @type LazySpec
 return {
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
 	dependencies = { "rafamadriz/friendly-snippets" },
 	event = "InsertEnter",
 
@@ -13,8 +12,6 @@ return {
 	--- @module "blink-cmp"
 	--- @type blink.cmp.Config
 	opts = {
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		-- keymap = { preset = "default" },
 		keymap = {
 			preset = "default",
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
@@ -43,17 +40,13 @@ return {
 			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
 			["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-			-- TODO: Does not appear to work?
 			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
 		},
 
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
 
-		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
 			ghost_text = {
 				enabled = true,
@@ -61,9 +54,6 @@ return {
 			list = {
 				selection = {
 					preselect = false,
-					-- preselect = function(ctx)
-					-- 	return vim.bo.filetype ~= "markdown"
-					-- end,
 				},
 			},
 			documentation = {
@@ -98,8 +88,6 @@ return {
 			},
 		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = {
 				"lazydev",
@@ -112,7 +100,6 @@ return {
 				lazydev = {
 					name = "LazyDev",
 					module = "lazydev.integrations.blink",
-					-- make lazydev completions top priority (see `:h blink.cmp`)
 					score_offset = 100,
 				},
 			},
@@ -122,11 +109,6 @@ return {
 			enabled = false,
 		},
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 	opts_extend = { "sources.default" },
