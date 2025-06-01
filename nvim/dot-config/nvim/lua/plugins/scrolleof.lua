2bbf10c370046d87f582a16e500d93ddf84dcd5c
Dreaded_X <tim@huizinga.dev>
2025-06-01 14:45:26 +0200

    Added scroll eof plugin

---
+++ nvim/dot-config/nvim/lua/plugins/scrolleof.lua
@@ -0,0 +1,8 @@
+-- https://github.com/Aasim-A/scrollEOF.nvim
+--- @module "lazy"
+--- @type LazySpec
+return {
+	"Aasim-A/scrollEOF.nvim",
+	event = { "CursorMoved", "WinScrolled" },
+	opts = {},
+}
