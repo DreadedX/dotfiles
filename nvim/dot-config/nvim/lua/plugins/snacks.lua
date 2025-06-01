707010aadc33f4aeec5bae0ac2cf74f64a152632
Dreaded_X <tim@huizinga.dev>
2025-06-01 19:43:28 +0200

    Use bufdel from snacks

--- nvim/dot-config/nvim/lua/plugins/snacks.lua
+++ nvim/dot-config/nvim/lua/plugins/snacks.lua
@@ -9,5 +9,23 @@ return {
 		--- @module "snacks"
 		--- @type snacks.Config
 		opts = {},
+		keys = {
+			{
+				"<leader>bd",
+				function()
+					Snacks.bufdelete.delete()
+				end,
+				silent = true,
+				desc = "Buffer delete",
+			},
+			{
+				"<leader>bD",
+				function()
+					Snacks.bufdelete.other()
+				end,
+				silent = true,
+				desc = "Buffer delete others",
+			},
+		},
 	},
 }
