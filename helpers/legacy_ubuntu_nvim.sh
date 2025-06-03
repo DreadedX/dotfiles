7be1fa7a2189f5188a122c64292278fa201ed63b
Dreaded_X <tim@huizinga.dev>
2025-06-03 22:08:27 +0200

    Added helper script for setting up neovim in legacy ubuntu devcontainer

---
+++ helpers/legacy_ubuntu_nvim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+sudo apt-get install fuse libfuse2 unzip ripgrep python3-venv -y --no-install-recommends
+sudo rm -rf /usr/local/bin/nvim
+sudo wget https://github.com/neovim/neovim-releases/releases/download/v0.11.1/nvim-linux-x86_64.appimage -O /usr/local/bin/nvim
+sudo chmod +x /usr/local/bin/nvim
+
+rm -rf ~/.dotfiles
+git clone https://git.huizinga.dev/Dreaded_X/dotfiles ~/.dotfiles
+ln -s ~/.dotfiles/nvim/dot-config/nvim ~/.config/nvim
