1f0acfed0138524aa9509a14ef3ae69df6b70952
Tim Huizinga <tim.huizinga@alten.nl>
2026-03-10 17:57:06 +0100

    Added another AIM yaml injection

--- nvim/dot-config/nvim/queries/yaml/injections.scm
+++ nvim/dot-config/nvim/queries/yaml/injections.scm
@@ -7,3 +7,11 @@
     (block_scalar) @injection.content
     (#set! injection.language "dockerfile")
     (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_template
+  (#eq? @_template "entrypoint")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
